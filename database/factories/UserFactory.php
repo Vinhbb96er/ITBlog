30a28813fabac93304f2f37b3caa0423b1cab7ff
Vinhbb96er <vinhbb96er@gmail.com>
2018-10-25 10:38:24 +0700

    fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,46 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'remember_token' => str_random(10),
+        'role' => $faker->numberBetween(1, 3),
+    ];
+});
+
+$factory->define(App\Models\UserDetail::class, function (Faker $faker) {
+    return [
+        'user_id' => \App\Models\User::all()->random()->id,
+        'full_name' => $faker->name,
+        'rate' => $faker->randomFloat(null, 0, 100),
+    ];
+});
+
+$factory->define(App\Models\Category::class, function (Faker $faker) {
+    return [
+        'name' => implode($faker->words(2), ' '),
+        'parent_id' => 1,
+    ];
+});
+
+$factory->define(App\Models\Post::class, function (Faker $faker) {
+    return [
+        'user_id' => \App\Models\User::all()->random()->id,
+        'category_id' => \App\Models\Category::all()->random()->id,
+        'title' => $faker->sentence(),
+        'preview' => $faker->sentence(2),
+        'content' => $faker->paragraph(2),
+        'total_view' => $faker->randomDigit(),
+        'total_like'=> $faker->randomDigit(),
+    ];
+});
+
+$factory->define(App\Models\Comment::class, function (Faker $faker) {
+    return [
+        'user_id' => \App\Models\User::all()->random()->id,
+        'post_id' => \App\Models\Post::all()->random()->id,
+        'content' => $faker->sentence(),
     ];
 });
