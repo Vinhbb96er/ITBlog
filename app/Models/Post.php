e16208bcbaec667f20c15bb7617bb4e8572bbc5b
Vinhbb96er <vinhbb96er@gmail.com>
2018-10-25 10:06:41 +0700

    create migration and model

---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'preview',
+        'content',
+        'total_view',
+        'total_like',
+    ];
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() 
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function category() 
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
